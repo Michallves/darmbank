da2a3c93ffc05ca0c8348e4d2dd319dad18b0f03
Michael Alves <maicon159951@outlook.com>
2022-12-12 21:31:57 -0300

    Update user_model.dart

--- lib/app/models/user_model.dart
+++ lib/app/models/user_model.dart
@@ -1,9 +1,38 @@
 class UserModel {
+  final String? userId;
   final String? name;
   final String? email;
   final String? cpf;
-  final String? image;
   final String? telephone;
+  final String? accountType;
+  final String? imageUrl;
 
-  UserModel({this.name, this.email, this.cpf, this.image, this.telephone});
+  UserModel(
+      {this.userId,
+      this.name,
+      this.email,
+      this.cpf,
+      this.imageUrl,
+      this.telephone,
+      this.accountType});
+
+  factory UserModel.fromMap(Map<String, dynamic> json) => UserModel(
+        userId: json["userId"],
+        name: json["name"],
+        email: json["email"],
+        cpf: json["cpf"],
+        telephone: json["telephone"],
+        accountType: json["accountType"],
+        imageUrl: json["imageUrl"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "userId": userId,
+        "name": name,
+        "email": email,
+        "cpf": cpf,
+        "telephone": telephone,
+        "accountType": accountType,
+        "imageUrl": imageUrl,
+      };
 }
